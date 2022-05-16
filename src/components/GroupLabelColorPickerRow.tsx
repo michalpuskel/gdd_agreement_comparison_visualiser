5c11c8ff0f79a853e4d16a53c222deea14183fb4
Michal Puškel <puskel@freevision.sk>
2022-05-16 19:08:52 +0200

    add styles for color pickers

--- src/components/GroupLabelColorPickerRow.tsx
+++ src/components/GroupLabelColorPickerRow.tsx
@@ -20,37 +20,39 @@ const GroupLabelColorPickerRow = ({ groupLabel, data, onChange }: IProps) => {
   return (
     <div className="GroupLabelColorPickerRow">
       <span className="GroupLabelColorPickerRow__Name">{groupLabel}</span>
-      <label>
-        background:
-        <input
-          type="color"
-          value={background}
-          onChange={(e) => {
-            setBackground(e.target.value);
-            onChange((prevColors) => ({
-              ...prevColors,
-              [groupLabel]: { background, text },
-            }));
-          }}
-        />
-      </label>
-      <label>
-        text:
-        <input
-          type="color"
-          value={text}
-          onChange={(e) => {
-            setText(e.target.value);
-            onChange((prevColors) => ({
-              ...prevColors,
-              [groupLabel]: { background, text },
-            }));
-          }}
-        />
-      </label>
-      <button title="remove group" onClick={handleRemove}>
-        ❌
-      </button>
+      <div className="GroupLabelColorPickerRow__Controls">
+        <label>
+          background:
+          <input
+            type="color"
+            value={background}
+            onChange={(e) => {
+              setBackground(e.target.value);
+              onChange((prevColors) => ({
+                ...prevColors,
+                [groupLabel]: { background, text },
+              }));
+            }}
+          />
+        </label>
+        <label>
+          text:
+          <input
+            type="color"
+            value={text}
+            onChange={(e) => {
+              setText(e.target.value);
+              onChange((prevColors) => ({
+                ...prevColors,
+                [groupLabel]: { background, text },
+              }));
+            }}
+          />
+        </label>
+        <button title="remove group" onClick={handleRemove}>
+          ❌
+        </button>
+      </div>
     </div>
   );
 };
